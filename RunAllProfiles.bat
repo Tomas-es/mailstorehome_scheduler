c2113e838468f37ab95c659b5e6530900762d53e
Tomas-es <officeremoto@gmail.com>
2025-10-27 18:50:16 +0100

    Close-MainWindow move to end out of for loop

--- RunAllProfiles.bat
+++ RunAllProfiles.bat
@@ -109,12 +109,15 @@ for %%P in (%PROFILES%) do (
     rem Send email log. Inside the loop to send after each profile.
     rem powershell.exe -ExecutionPolicy Bypass -File "%MAILSCRIPT%" -ProfileName "%%~P" -Result "%RESULT%" -LogFile "!LOGFILE!"
     echo "-ExecutionPolicy Bypass -File "%MAILSCRIPT%" -ProfileName "%%~P" -Result "%RESULT%" -LogFile "!LOGFILE!""
-    powershell.exe -ExecutionPolicy Bypass -File .\close-MainWindow.ps1 -ProcessName "MailStoreHome" -WaitSeconds 30
-    if errorlevel 1 (
-      echo WARNING: Close-MainWindow.ps1 reported an error >> "!LOGFILE!" 2>&1
-    ) else (
-      echo Close-MainWindow.ps1 completed successfully >> "!LOGFILE!" 2>&1
-    )
 )
+
+# Out of the loop
+# Is enough to close once
+powershell.exe -ExecutionPolicy Bypass -File .\close-MainWindow.ps1 -ProcessName "MailStoreHome" -WaitSeconds 30
+    if errorlevel 1 (
+      echo WARNING: Close-MainWindow.ps1 reported an error
+    ) else (
+      echo Close-MainWindow.ps1 completed successfully
+    )
 
 endlocal
