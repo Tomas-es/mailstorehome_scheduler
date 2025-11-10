0ddedc5f35cc7db4f172b4535873ed3b88939780
Tomas-es <officeremoto@gmail.com>
2025-11-10 23:42:19 +0100

    First steps attachments

--- Test-SendMail.ps1
+++ Test-SendMail.ps1
@@ -10,7 +10,7 @@ $email = $config.email
 $destinationEmail = $config.destinationEmail
 $smtpServer = $config.smtpServer
 $smtpPort = $config.smtpPort
-$PasswordPath = $config.PasswordPath
+$passwordPath = $config.passwordPath
 
 # Create the message
 $message = New-Object MimeKit.MimeMessage
@@ -21,13 +21,20 @@ $message.Subject = "Hello from PowerShell + MailKit"
 $builder = New-Object MimeKit.BodyBuilder
 $builder.TextBody = "This is a plain text message."
 $builder.HtmlBody = "<h2>This is HTML</h2><p>Sent via MailKit</p>"
+<#
+    Attachments is a collection.
+    Add files using the Add() method.
+    ToDo: Create a file with all the atachments
+    or use a loop to add multiple files.
+#>
+$builder.Attachments.Add(".\Close-MainWindow.log")
 $message.Body = $builder.ToMessageBody()
 
 # Send the message
 $client = New-Object MailKit.Net.Smtp.SmtpClient
 $client.Connect($smtpServer, $smtpPort, [MailKit.Security.SecureSocketOptions]::SslOnConnect)
 # Secure modifiation to read password from file
-$securePassword = Get-Content -Path $PasswordPath | ConvertTo-SecureString
+$securePassword = Get-Content -Path $passwordPath | ConvertTo-SecureString
 $credential = New-Object System.Management.Automation.PSCredential ($email, $securePassword)
 $client.Authenticate($credential.UserName, $credential.GetNetworkCredential().Password)
 $client.Send($message)
